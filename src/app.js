367565263d2b32d9a99efcf64dc6aac1899d39cf
Sergio Cruz <hello@sergiocruz.me>
2016-04-14 15:49:01 -0400

    Add routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import jQuery from 'jquery';
-import CommentBox from './components/comment-box'
 
+import {Router, Route, Redirect} from 'react-router';
+
+import Layout from './layout/layout'
+
+import BlogPage from './pages/blog'
+import PicturePage from './pages/picture'
+import VideoPage from './pages/video'
+
+const app = (
+  <Router>
+    <Redirect from="/" to="/blog" />
+    <Route path="/" component={Layout}>
+      <Route path="blog" component={BlogPage} />
+      <Route path="picture" component={PicturePage} />
+      <Route path="video" component={VideoPage} />
+    </Route>
+  </Router>
+)
 
 jQuery(function() {
   ReactDOM.render(
-    <CommentBox />,
+    app,
     document.getElementById('comment-box'),
     function() {
       console.timeEnd('react-app')
