62f756abc32aa837a6342a0a3b32a176a76d9bde
Stephen Schroeder <schrode50@gmail.com>
2016-10-14 11:27:39 -0700

    breaking methods out into individual files for modularity

---
+++ src/components/comment_form.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default class CommentForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: 0
+    };
+  }
+
+  render() {
+    return (
+      <form className="comment-form" onSubmit={this._handleSubmit.bind(this)}>
+        <label>New comment</label>
+        <div className="comment-form-fields">
+          <input placeholder="Name:" ref={c => this._author = c} />
+          <textarea placeholder="Comment:" ref={c => this._body = c} onChange={this._getCharacterCount.bind(this)}></textarea>
+        </div>
+        <p>{this.state.characters} characters</p>
+        <div className="comment-form-actions">
+          <button type="submit">
+            Post comment
+          </button>
+        </div>
+      </form>
+    );
+  }
+
+  _getCharacterCount() {
+
+    this.setState({
+      characters: this._body.value.length
+    });
+
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.addComment(this._author.value, this._body.value);
+
+    this._author.value = '';
+    this._body.value = '';
+
+    this.setState({ characters: 0  });
+  }
+}
